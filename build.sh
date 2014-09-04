842769d335f5d47397afa299f8216d4a401268d7
Nick O'Dell <nickodell@gmail.com>
2014-09-03 23:30:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Dependencies
-apt-get update
-apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make
-dpkg -i libdb4.8_4.8.24-1ubuntu1_i386.deb
-dpkg -i libdb4.8-dev_4.8.24-1ubuntu1_i386.deb
-dpkg -i libdb4.8++_4.8.24-1ubuntu1_i386.deb
-dpkg -i libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb
+apt-get update || exit
+apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make || exit
+dpkg -i libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i libdb4.8-dev_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i libdb4.8++_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb || exit
 
 cd /root
 rm -rf bitcoin
