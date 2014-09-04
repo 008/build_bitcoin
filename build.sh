9bbfc9c7fe150eae80bd435c39676300422d9e30
root <root@d1stkfactory>
2014-09-04 05:08:57 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Dependencies
+apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make
+dpkg -i libdb4.8_4.8.24-1ubuntu1_i386.deb
+dpkg -i libdb4.8-dev_4.8.24-1ubuntu1_i386.deb
+dpkg -i libdb4.8++_4.8.24-1ubuntu1_i386.deb
+dpkg -i libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb
+
+cd /root
+rm -rf bitcoin
+git clone https://github.com/bitcoin/bitcoin.git
+cd /root/bitcoin
+./autogen.sh
+./configure
+make
+
