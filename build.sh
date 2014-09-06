23fa4dc40fac1e412679e2c546e96c436801e93c
Nick O'Dell <nickodell@gmail.com>
2014-09-05 20:48:36 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Dependencies
+cd /root/build_bitcoin
 apt-get update || exit
 apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make libminiupnpc-dev || exit
 dpkg -i libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
