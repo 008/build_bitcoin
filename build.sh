ec0bc6d5a5eb8b546bc492b946eb20a89b0d1957
Nick ODell <nickodell@gmail.com>
2014-09-12 17:13:39 -0600

    Alter build script to avoid absolute paths

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
+# Needs root
+if [ `id -u` != 0 ] ; then
+	echo "Need root!"
+	exit
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 # Dependencies
-cd /root/build_bitcoin
 apt-get update || exit
 apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make libminiupnpc-dev || exit
-dpkg -i libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i libdb4.8-dev_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i libdb4.8++_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb || exit
+# Can't find libdb 4.8 in debian repos
+dpkg -i $SCRIPTPATH/libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8-dev_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8++_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb || exit
 
-cd /root
-rm -rf bitcoin
 git clone https://github.com/bitcoin/bitcoin.git
-cd /root/bitcoin
+cd bitcoin
 ./autogen.sh
 ./configure
 make
